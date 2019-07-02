cdad60e7aa2004ae80ec5c68056e636fd229cfae
Antony Okilipa <okilipa.antony@intsoftkenya.co.ke>
2019-07-02 09:25:10 +0300

    initialise react native app

---
+++ n52_week_challenge_reactN/android/app/src/main/java/com/n52_week_challenge_reactn/MainActivity.java
@@ -0,0 +1,15 @@
+package com.n52_week_challenge_reactn;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "n52_week_challenge_reactN";
+    }
+}
